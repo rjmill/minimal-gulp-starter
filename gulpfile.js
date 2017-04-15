7f1137013ef134cc1276e12129110248c0571a60
RJ Miller <rjmiller10@gmail.com>
2017-04-15 14:37:06 -0400

    Reload on changes in any files in app/

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,7 @@ gulp.task('serve', function() {
     }
   });
 
-  gulp.watch('app/css/**/*.css').on('change', browserSync.reload);
-  gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
-  // TODO: figure out a better way to do the HTML part of this
-  gulp.watch('app/**/*.html').on('change', browserSync.reload);
+  gulp.watch('app/**/*').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
